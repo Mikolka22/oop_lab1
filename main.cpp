e77edd031a12d89d83646b81ce895444f53260a4
Mikolka22 <kolasmirnagin208@gmail.com>
2026-09-25 15:15:45 +0300

    Extract AppManager and document four-file layout

--- main.cpp
+++ main.cpp
@@ -1,55 +1,6 @@
-#define NOMINMAX
-#include <windows.h>
-#include <conio.h>
-#include <cstdlib>
 #include <iostream>
 #include <string>
-#include "Line.h"
-
-// Менеджер выбирает место и цвет линии, управляет консолью и скоростью.
-class AppManager {
-    int speed, length;
-    bool colorful;
-
-public:
-    AppManager(int speed, int length, bool colorful)
-        : speed(speed), length(length), colorful(colorful) {}
-
-    bool run() const {
-        HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_SCREEN_BUFFER_INFO info{};
-        if (!GetConsoleScreenBufferInfo(console, &info)) return false;
-        WORD normal = info.wAttributes;
-        DWORD written;
-        DWORD cells = static_cast<DWORD>(info.dwSize.X) * info.dwSize.Y;
-        FillConsoleOutputCharacterW(console, L' ', cells, COORD{0, 0}, &written);
-        FillConsoleOutputAttribute(console, normal, cells, COORD{0, 0}, &written);
-
-        std::srand(GetTickCount());
-        auto stop = [] {
-            return (GetAsyncKeyState(VK_ESCAPE) & 0x8000) ||
-                   (_kbhit() && _getch() == 27);
-        };
-
-        while (!stop()) {
-            if (!GetConsoleScreenBufferInfo(console, &info) ||
-                info.srWindow.Right - info.srWindow.Left < 3) return false;
-
-            int x = info.srWindow.Left + 1 +
-                    std::rand() % (info.srWindow.Right - info.srWindow.Left - 2);
-            WORD color = colorful ? static_cast<WORD>(1 + std::rand() % 15)
-                                 : WORD(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-            Line line(x, info.srWindow.Top, info.srWindow.Bottom, length, color);
-
-            while (!line.finished()) {
-                if (stop()) return true;
-                line.step(console, normal);
-                Sleep(1000 / speed);
-            }
-        }
-        return true;
-    }
-};
+#include "AppManager.h"
 
 int main(int argc, char* argv[]) {
     SetConsoleOutputCP(CP_UTF8);
